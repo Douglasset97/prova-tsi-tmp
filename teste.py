01ca705b5c5ece2b6faca97b00745723f7c2fd82
douglas silva <douglassilva11@hotmail.com>
2024-12-30 09:38:42 -0300

    alteração feita

--- teste.py
+++ teste.py
@@ -1,10 +1,10 @@
 import ollama
 import gensim.downloader as api
 
-# Carregar o modelo de Word2Vec pré-treinado
-model = api.load("word2vec-google-news-300")  # Modelo com maior capacidade para capturar relações semânticas complexas
+# Carregar o modelo de Word2Vec 
+model = api.load("word2vec-google-news-300")
 
-# Realizar a operação correta usando palavras positivas e negativas
+# Realizar a operação correta usando as palavras positivas e negativas
 resultado = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=5)
 
 # Imprimir resultados
